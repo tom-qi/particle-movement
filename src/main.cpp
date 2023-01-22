a19c46b6907c402f2526c8b36965f23b9dd8cbfd
Tom <q.tom08@gmail.com>
2023-01-22 02:22:48 -0500

    add textures, renderers, and buffers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+using namespace std;
+
 int main()
 {
 
@@ -18,6 +20,32 @@ int main()
     SDL_Quit();
     return 2;
   }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC); //sync renderer with vertical refresh
+  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+  
+  if (renderer == NULL){
+    cout << "could not create renderer" << endl;
+    SDL_DestroyWindow(window); 
+    SDL_Quit();
+    return 3;
+  }
+  if (texture == NULL){
+    cout << "could not create texture" << endl;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window); 
+    SDL_Quit();
+    return 4;
+  }
+
+  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT]; //allocate memory to store pixels
+  // might be  m_buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT]
+
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32)); //update the texture with the info in the buffer and tell the renderer to render and draw the texture
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer); 
+  
   bool quit = false;
 
   SDL_Event event;
@@ -34,6 +62,9 @@ int main()
     }
   }
 
+  delete [] buffer;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(texture);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
